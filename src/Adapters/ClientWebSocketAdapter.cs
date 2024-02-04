638f79a34d396eae8af0626d04ef891157dbbac1
Johan Ahlbäck <ahlback.johan@gmail.com>
2024-02-05 00:47:39 +0100

    Add Port configuration and fix websocket connection.  fixes #6 (#8)
    * Added port configuration and fixed websocket connection

--- src/Adapters/ClientWebSocketAdapter.cs
+++ src/Adapters/ClientWebSocketAdapter.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Teams.ThirdPartyAppApi.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 namespace Teams.ThirdPartyAppApi.Adapters;
-internal interface IClientWebSocket
+internal interface IClientWebSocket : IDisposable
 {
     WebSocketState State { get; }
     Task ConnectAsync(Uri uri, CancellationToken cancellationToken);
@@ -13,39 +13,53 @@ internal interface IClientWebSocket
     Task CloseAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken);
     Task CloseOutputAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken);
     void Abort();
+    void CreateNewSocket();
     WebSocketCloseStatus? CloseStatus { get; }
     string? CloseStatusDescription { get; }
 }
 
-internal class ClientWebSocketAdapter : IClientWebSocket
+internal class ClientWebSocketAdapter : IClientWebSocket, IDisposable
 {
-    private readonly ClientWebSocket _clientWebSocket;
+    private ClientWebSocket _clientWebSocket;
 
     public ClientWebSocketAdapter()
     {
         _clientWebSocket = new ClientWebSocket();
     }
 
+    public void CreateNewSocket()
+    {
+        try { _clientWebSocket.Dispose(); }
+        catch { }
+
+        _clientWebSocket = new ClientWebSocket();
+    }
     public WebSocketState State => _clientWebSocket.State;
     public WebSocketCloseStatus? CloseStatus => _clientWebSocket.CloseStatus;
     public string? CloseStatusDescription => _clientWebSocket.CloseStatusDescription;
 
-    public Task ConnectAsync(Uri uri, CancellationToken cancellationToken) => 
+    public Task ConnectAsync(Uri uri, CancellationToken cancellationToken) =>
         _clientWebSocket.ConnectAsync(uri, cancellationToken);
 
-    public Task SendAsync(ArraySegment<byte> buffer, WebSocketMessageType messageType, bool endOfMessage, CancellationToken cancellationToken) => 
+    public Task SendAsync(ArraySegment<byte> buffer, WebSocketMessageType messageType, bool endOfMessage, CancellationToken cancellationToken) =>
         _clientWebSocket.SendAsync(buffer, messageType, endOfMessage, cancellationToken);
 
-    public Task<WebSocketReceiveResult> ReceiveAsync(ArraySegment<byte> buffer, CancellationToken cancellationToken) => 
+    public Task<WebSocketReceiveResult> ReceiveAsync(ArraySegment<byte> buffer, CancellationToken cancellationToken) =>
         _clientWebSocket.ReceiveAsync(buffer, cancellationToken);
 
-    public Task CloseAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken) => 
+    public Task CloseAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken) =>
         _clientWebSocket.CloseAsync(closeStatus, statusDescription, cancellationToken);
 
-    public Task CloseOutputAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken) => 
+    public Task CloseOutputAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken) =>
         _clientWebSocket.CloseOutputAsync(closeStatus, statusDescription, cancellationToken);
 
-    public void Abort() => 
+    public void Abort() =>
         _clientWebSocket.Abort();
+
+    public void Dispose()
+    {
+        try { _clientWebSocket.Dispose(); }
+        catch { }
+    }
 }
 
